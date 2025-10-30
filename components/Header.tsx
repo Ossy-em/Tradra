6d40a92d7ea60b766f3b5a92a18c1171c80bc28d
Ossy-em <emosinachi@gmail.com>
2025-10-30 23:52:50 +0100

    Watchlist

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/components/NavItems";
-import UserDropdown from "@/components/userDropdown";
+import UserDropdown from "@/components/UserDropdown";
 import {searchStocks} from "@/lib/actions/finnhub.actions";
 
 const Header = async ({ user }: { user: User }) => {
